b8856915148b6314913cdc578dc06f1a4306b0d0
Aditya Vikram Bal <kanhabal07@gmail.com>
2026-08-11 18:18:50 +0530

    Updated backend live URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://kishandrishti.onrender.com';
 
 // 1. Weather Advisory API (MandiWeather.jsx)
 export const getWeather = async (location) => {
